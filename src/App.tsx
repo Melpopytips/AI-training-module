d7e4f061b45adba9837020b842acf7d8ccc4d23e
Melpopytips <melpopytips@gmail.com>
2025-05-20 14:44:03 +0700

    Fix React Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FormationContent from './FormationContent';
+import FormationDashboard from './FormationDashboard';
+import QuizAnalysis from './QuizAnalysis';
 
 function App() {
   return (
-    <BrowserRouter>
-      <FormationContent />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FormationContent />} />
+        <Route path="/dashboard" element={<FormationDashboard />} />
+        <Route path="/analysis/:submissionId" element={<QuizAnalysis />} />
+      </Routes>
+    </Router>
   );
 }
 
